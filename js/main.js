dbac8f80e373b080ab7dc810d6417d6ed37dde89
diana_marmol <dianamarmolus@gmail.com>
2023-09-07 11:36:33 +0200

    Added variables for cats photo, name, description and race

--- js/main.js
+++ js/main.js
@@ -1,49 +1,72 @@
-' use strict';
-const jsList = document.querySelector(".js-list")
+" use strict";
+const jsList = document.querySelector(".js-list");
+
+/* tarjeta 1 */
+const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
+const kittenName1 = "Anastacio";
+const kittenDesc1 =
+  "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+const kittenRace1 = "Siamés";
+
+/* tarjeta 2 */
+const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
+const kittenName2 = "Fiona";
+const kittenDesc2 =
+ "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+const kittenRace2 = "Sphynx";
+
+/* tarjeta 3 */
+const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
+const kittenName3 = "Cielo";
+const kittenDesc3 =
+ "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+const kittenRace3 = "Sphynx";
 
 const kittenOne = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
-    alt="gatito"
-  />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
-   </p>
-</article>
-</li>`;
- const kittenTwo = `<li class="card">
+          <article>
             <img
               class="card_img"
-              src="https://dev.adalab.es/sphynx-gato.webp"
+              src="${kittenImage1}"
+              alt="gatito"
+            />
+            <h3 class="card_title">${kittenName1.toUpperCase()}</h3>
+            <h4 class="card_race">${kittenRace1}</h4>
+            <p class="card_description">
+                ${kittenDesc1}
+            </p>
+          </article>
+        </li>`;
+
+/* console.log(kittenImage1 + kittenName1 + kittenRace1 + kittenDesc1);  */
+
+const kittenTwo = `<li class="card">
+         <article>
+            <img
+              class="card_img"
+              src="${kittenImage2}"
               alt="sphynx-cat"
             />
-            <h3 class="card_title">Fiona</h3>
-            <h4 class="card_race">Sphynx</h4>
+            <h3 class="card_title">${kittenName2.toUpperCase()}</h3>
+            <h4 class="card_race">${kittenRace2}</h4>
             <p class="card_description">
-              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-              hasta con pinta de alienígena han llegado a definir a esta raza
-              gatuna que se caracteriza por la «ausencia» de pelo.
+            ${kittenDesc2}
             </p>
+            </article>
           </li>`;
- const kittenThree = `<li class="card">
+
+const kittenThree = `<li class="card">
+          <article>
             <img
               class="card_img"
-              src="https://dev.adalab.es/maine-coon-cat.webp"
+              src="${kittenImage3}"
               alt="maine-coon-cat"
             />
-            <h3 class="card_title">Cielo</h3>
-            <h4 class="card_race">Maine Coon</h4>
+            <h3 class="card_title">${kittenName3.toUpperCase()}</h3>
+            <h4 class="card_race">${kittenRace3}</h4>
             <p class="card_description">
-              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-              bella mirada se ha convertido en una de sus señas de identidad.
-              Sus ojos son grandes y las orejas resultan largas y en punta.
+            ${kittenDesc3}
             </p>
-          </li>`;
- 
-      jsList.innerHTML = kittenOne + kittenTwo + kittenThree;
+          </article>
+        </li>`;
+
+jsList.innerHTML = kittenOne + kittenTwo + kittenThree;
