0f4fd9eee6b309179640cb370ff3462fa9400751
diana_marmol <dianamarmolus@gmail.com>
2023-09-14 11:29:39 +0200

    Fixed search

--- js/main.js
+++ js/main.js
@@ -1,106 +1,182 @@
 " use strict";
-const input_search_desc = document.querySelector('.js_insearch_desc');
-const descrSearchText = input_search_desc.value;
+
+/* Variables */
+
+const buttonSearch = document.querySelector('.js-button-search');
+
+/* descripción */
+const input_search_desc = document.querySelector('.js_in_search_desc');
+
+/* raza */
 const input_search_race = document.querySelector('.js_search_race');
-const raceSearchText = input_search_race.value;
 
 
-const jsList = document.querySelector(".js-list");
+const newForm = document.querySelector('.new-form');
+const btnPlus = document.querySelector('.js-btn-plus');
+
+const btnAdd = document.querySelector(".js-btn-add");
+
+const listElement = document.querySelector(".js-list");
+
 
 /* tarjeta 1 */
-const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
+const kittenData_1 = {
+  image: 'https://dev.adalab.es/gato-siames.webp',
+  name: 'Anastacio',
+  desc: ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: 'siamés',
+};
+
+/* const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
 const kittenName1 = "Anastacio";
 const kittenDesc1 =
   "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
-const kittenRace1 = "Siamés";
+const kittenRace1 = "siamés"; */
 
 /* tarjeta 2 */
-const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
+const kittenData_2 = {
+  image: 'https://dev.adalab.es/sphynx-gato.webp',
+  name: 'Fiona',
+  desc:
+ "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.",
+  race:'sphynx'
+};
+
+/* const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
 const kittenName2 = "Fiona";
 const kittenDesc2 =
  "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
-const kittenRace2 = "Sphynx";
+const kittenRace2 = "sphynx"; */
 
 /* tarjeta 3 */
-const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
+
+const kittenData_3 = {
+  image: 'https://dev.adalab.es/maine-coon-cat.webp',
+  name: 'Cielo',
+  desc:  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.",
+  race: 'maine coon',
+};
+
+/* const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
 const kittenName3 = "Cielo";
 const kittenDesc3 =
  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
-const kittenRace3 = "Maine coon";
+const kittenRace3 = "maine coon"; */
 
-let html = '';
+/* let html = ''; */
 
-if (kittenRace1 === "" ) {
+/* if (kittenRace1 === "" ) {
   html = `Uy que despiste, no sabemos su raza`;
 } else {
   html = kittenRace1;
 }; 
+ */
 
 const kittenOne = `<li class="card">
           <article>
             <img
               class="card_img"
-              src="${kittenImage1}"
+              src="${kittenData_1.image}"
               alt="gatito"
             />
-            <h3 class="card_title">${kittenName1.toUpperCase()}</h3>
-            <h4 class="card_race">${html}</h4>
+            <h3 class="card_title">${kittenData_1.name.toUpperCase()}</h3>
+            <h4 class="card_race">${kittenData_1.race}</h4> 
             <p class="card_description">
-                ${kittenDesc1}
+                ${kittenData_1.desc}
             </p>
           </article>
         </li>`;
 
 /* console.log(kittenImage1 + kittenName1 + kittenRace1 + kittenDesc1);  */
 
-if (kittenRace2 === "" ) {
+/* if (kittenRace2 === "" ) {
   html = `Uy que despiste, no sabemos su raza`;
 } else {
   html = kittenRace2;
-}; 
+}; */ 
 
 const kittenTwo = `<li class="card">
          <article>
             <img
               class="card_img"
-              src="${kittenImage2}"
+              src="${kittenData_2.image}"
               alt="sphynx-cat"
             />
-            <h3 class="card_title">${kittenName2.toUpperCase()}</h3>
-            <h4 class="card_race">${html}</h4>
+            <h3 class="card_title">${kittenData_2.name.toUpperCase()}</h3>
+            <h4 class="card_race">${kittenData_2.race}</h4>
             <p class="card_description">
-            ${kittenDesc2}
+            ${kittenData_2.desc}
             </p>
             </article>
           </li>`;
 
-if (kittenRace3 === "" ) {
+/* if (kittenRace3 === "" ) {
   html = `Uy que despiste, no sabemos su raza`;
 } else {
   html = kittenRace3;
-}; 
+};  */
 
 const kittenThree = `<li class="card">
           <article>
             <img
               class="card_img"
-              src="${kittenImage3}"
+              src="${kittenData_3.image}"
               alt="maine-coon-cat"
             />
-            <h3 class="card_title">${kittenName3.toUpperCase()}</h3>
-            <h4 class="card_race">${html}</h4>
+            <h3 class="card_title">${kittenData_3.name.toUpperCase()}</h3>
+            <h4 class="card_race">${kittenData_3.race}</h4>
             <p class="card_description">
-            ${kittenDesc3}
+            ${kittenData_3.desc}
             </p>
           </article>
         </li>`;
 
 /*  OPCIÓN 2
 <h4 class="card_race">${kittenRace3 === ""? `Uy que despiste, no sabemos su raza` : kittenRace3}</h4> */
-const newForm=document.querySelector('.new-form');
-const btnPlus = document.querySelector('.js-btn-plus');
-btnPlus.addEventListener('click', handleClickNewCatForm); 
-    
+
+
+/* Funciones */
+
+/* función manejadora del evento */
+const filterKitten = (event) => {
+  event.preventDefault();
+  const descrSearchText = input_search_desc.value;
+  const raceSearchText = input_search_race.value;
+
+  listElement.innerHTML = ""; /* resetea lo anterior */
+
+  if (descrSearchText !== "" ){
+        // falta si no hay descripción ""
+    if (kittenData_1.desc.includes(descrSearchText)) {
+      listElement.innerHTML += kittenOne;
+    } else if (kittenData_2.desc.includes(descrSearchText)) {
+      listElement.innerHTML += kittenTwo;
+    } else if (kittenData_3.desc.includes(descrSearchText)) {
+      listElement.innerHTML += kittenThree;
+    };
+  } /* else {
+    listElement.innerHTML = `Uy que despiste, no sabemos su descripción`;
+  } */
+
+  renderRace(raceSearchText);
+};
+
+const renderRace = (raceSearchText) => {
+
+  if (raceSearchText !== "" ){
+    if (kittenData_1.race.includes(raceSearchText.toLowerCase())) {
+      listElement.innerHTML += kittenOne;
+    } else if (kittenData_2.race.includes(raceSearchText.toLowerCase())) {
+      listElement.innerHTML += kittenTwo;
+    } else if (kittenData_3.race.includes(raceSearchText.toLowerCase())) {
+      listElement.innerHTML += kittenThree;
+    } 
+  } /* else {
+    listElement.innerHTML = `Uy que despiste, no sabemos su raza`;
+  }  */
+}
+
+
 function showNewCatForm() {
   newForm.classList.remove('collapsed');
 }
@@ -108,23 +184,14 @@ function hideNewCatForm() {
   newForm.classList.add('collapsed');
 }
 
-
 function handleClickNewCatForm(event){
   event.preventDefault();
      if ( newForm.classList.contains('collapsed')){
     showNewCatForm()
-    
    }
    else { hideNewCatForm()
-     
-
    }
   }
-
-
-  const btnAdd = document.querySelector(".js-btn-add");
-
-  btnAdd.addEventListener('click', addNewKitten );
 
   function addNewKitten(event) {
     event.preventDefault();
@@ -138,9 +205,6 @@ function handleClickNewCatForm(event){
     const valueDesc = inputDesc.value;
     const valuePhoto = inputPhoto.value;
     const valueName = inputName.value;
-    
-  
-
 
     if (valueDesc === '' || valuePhoto === '' || valueName === '') {
       labelMessageError.innerHTML = "¡Uy! parece que has olvidado algo";
@@ -151,22 +215,26 @@ function handleClickNewCatForm(event){
   };
 
 
-if( kittenDesc1.includes(descrSearchText) ) {
-  jsList.innerHTML+= kittenOne;
-  }
-  
- 
-  if( kittenDesc2.includes(descrSearchText) ) {
-  jsList.innerHTML+=kittenTwo;
-  }
-  
-  if( kittenDesc3.includes(descrSearchText) ) {
-    jsList.innerHTML+=kittenThree;
+  /*  AYUDA!!!  */
+  function renderKitten(kittenData) {
+   kittenData.image,
+
+
+/*     image: 'https://dev.adalab.es/gato-siames.webp',
+  name: 'Anastacio',
+  desc: ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: 'siamés', */
   }
 
 
 
-/*   || kittenRace2 === "" || kittenRace3 === ""  */
 
-//add menukitten remove menukitten
+/* listeners/events */
+btnPlus.addEventListener('click', handleClickNewCatForm); 
+btnAdd.addEventListener('click', addNewKitten );
+
+buttonSearch.addEventListener('click', filterKitten);
+
+/* const handleClickDescRace(filterKitten,renderRace); MAL*/
+
 
